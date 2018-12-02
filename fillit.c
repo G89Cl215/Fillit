a160373b1aee57e8c7d84876eeee43221218c617
Thomas GOUEDARD <tgouedar@e2r1p6.42.fr>
2018-12-02 18:51:06 +0100

    Le backtracking, Chapitre Deuxieme : Preparation des essais

--- fillit.c
+++ fillit.c
@@ -6,7 +6,7 @@
 /*   By: baavril <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/01 18:22:41 by baavril           #+#    #+#             */
-/*   Updated: 2018/12/02 13:13:55 by tgouedar         ###   ########.fr       */
+/*   Updated: 2018/12/02 18:49:38 by tgouedar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,27 +42,37 @@ int		ft_get_next_tetro(int fd, unsigned short **tab)
 	return (size);
 }
 
-int		main(int ac, char **av)
+unsigned short	ft_tab_tetro(int fd)
 {
 	unsigned short	*tab;
-	int				i = 0;
-	int				fd = 0;
+	unsigned short	*temp;
+	int				size;
 
-	if (!(tab = (unsigned short*)ft_memalloc(sizeof(*tab) * 26)))
-		return (0);
+	if (!(temp = (unsigned short*)ft_memalloc(sizeof(*tab) * 26)))
+		return (NULL);
+	while (size = ft_get_next_tetro(fd, &temp)) ;
+	if (!(tab = (unsigned short*)ft_memalloc(sizeof(*tab) * (size + 1))))
+		return (NULL);
+	tab[size] = 0;
+	while (size--)
+		tab[size] = temp[size];
+	free(temp);
+	return (tab);
+}
+
+
+int				main(int ac, char **av)
+{
+	unsigned short	*tab;
+	unsigned short	*map;
+
 	if (ac > 1)
 	{
 		fd = open(av[1], O_RDONLY);
-		while (i < 333 && ft_get_next_tetro(fd, &tab))
-		{
-			ft_putchar('+');
-	//		ft_putnbr_base(tab[i], "01");
-			ft_putchar('\n');
-			i++;
-		}
+		tab = ft_tab_tetro(fd);
+		close(fd);
+		map = ft_resolve(&tab);
 	}
-	(void)fd;
-	free(tab);
-	close(fd);
+
 	return (0);
 }
